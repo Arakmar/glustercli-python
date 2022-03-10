f87dbcb66a713e4c6a3c87fad0b07a365a847d7b
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2022-03-10 14:08:35 +0100

    Return only the version string from glusterfs_version

--- glustercli/cli/gluster_version.py
+++ glustercli/cli/gluster_version.py
@@ -6,4 +6,6 @@ def glusterfs_version():
     """Return the GlusterFS version"""
     cmd = ["--version"]
     # The library takes care of raising exception in case of any errors
-    return(gluster_execute(cmd).split('\n')[0])
+    version_line = gluster_execute(cmd).split('\n')[0]
+
+    return version_line.split(' ')[1]
